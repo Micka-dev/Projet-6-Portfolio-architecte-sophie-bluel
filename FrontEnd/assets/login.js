c9cd49c4655258dcfd2404c6543d2b07841d1bb9
Mickaël Douceré <mickamickadou@gmail.com>
2024-06-03 15:31:07 +0200

    Formulaire de connexion fonctionnel avec redirection vers la page d’accueil quand la connexion est confirmée + un message d’erreur quand les informations utilisateur / mot de passe ne sont pas correctes

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,31 @@
+// Gestion de l'écoute de l'évènement "submit" du formulaire
+const formLogin = document.querySelector(".logIn")
+formLogin.addEventListener("submit", async (event) => {
+    event.preventDefault()
+
+    // Création de l’objet login (charge utile)
+    const objectLogin = {
+        password: event.target.querySelector("[name=password").value,
+        email: event.target.querySelector("[name=email]").value,
+    }
+
+    // Création de l’objet login (charge utile) au format JSON
+    const bodyLogin = JSON.stringify(objectLogin);
+
+    // Appel de la fonction fetch avec toutes les informations nécessaires
+    const sending = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: bodyLogin
+    });
+    let response = await sending.json()
+    console.log(response)
+
+    // revoir si autre façon de faire que voir s'il existe un "token"
+    if (response.token) {
+        window.localStorage.setItem("token", response.token)
+        document.location.href = "http://127.0.0.1:5501/FrontEnd/"
+    } else {
+        alert("Erreur dans l’identifiant ou le mot de passe")
+    }
+})
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,20 +1,11 @@
+// Script type="module" définie dans page index html, ce qui permet d'utiliser await directement car on est déjà en asynchrone  )
 
-                                        //Récupération des works eventuellement stockées dans le localStorage
-                                        // let works = window.localStorage.getItem('works');
-                                        // if (works === null) {
-  
-// Récupération des "works" (projets) depuis l'API
+// Récupération des "works" (projets) depuis l'API. 
 
 const response = await fetch('http://localhost:5678/api/works');
-const works = await response.json();   // formate la reponse en json
- 
-                                        //     // Transformation des pièces en JSON
-                                        //     const valueWorks = JSON.stringify(works);
-                                        //     // Stockage des informations dans le localStorage
-                                        //     window.localStorage.setItem("works", valueWorks);
-                                        // } else {
-                                        //     works = JSON.parse(works);
-                                        // }
+
+// Formatage de la reponse en json
+const works = await response.json();
 
 
 // Fonction qui affiche tous les projets, les "works" sur la page web
@@ -23,7 +14,7 @@ function renderWorks(works) {
 
     // Création des balises
 
-   works.map((work)=>{
+    works.map((work) => {
 
         // Récupération de l'élément du DOM qui accueillera les works
         const gallery = document.querySelector(".gallery");
@@ -37,9 +28,8 @@ function renderWorks(works) {
 
         const figcaptionElement = document.createElement("figcaption");
         figcaptionElement.innerText = work.title;
-       
-        // Rattachement de nos balises au DOM
 
+        // Rattachement de nos balises au DOM
         // On rattache la balise figureElement à la div gallery
         gallery.appendChild(figureElement);
         figureElement.appendChild(imageElement);
@@ -49,6 +39,7 @@ function renderWorks(works) {
 }
 
 // Affichage des projets "works" sur la page web
+
 renderWorks(works)
 
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -56,7 +47,7 @@ renderWorks(works)
 // Récupération des "categories" (filtres) depuis l'API
 
 const answer = await fetch('http://localhost:5678/api/categories');
-const categories = await answer.json();   // formate la reponse en json
+const categories = await answer.json();
 
 function renderFilters(categories) {
 
@@ -68,32 +59,32 @@ function renderFilters(categories) {
     // Création d’une balise dédiée au filtre "Tous" (resetFilters)
     const buttonElement = document.createElement("button");
     buttonElement.innerText = "Tous"
-    buttonElement.dataset.id = "0"  /////
+    buttonElement.dataset.id = "0"
     buttonElement.classList.add("filtersButton", "filtersButtonActivated")
     filters.appendChild(buttonElement)
 
-    categories.map((category)=>{
+    categories.map((category) => {
 
         // Création d’une balise dédiée à un filtre
         const buttonElement = document.createElement("button");
         buttonElement.innerText = category.name;
         buttonElement.dataset.id = category.id
         buttonElement.classList.add("filtersButton")
-              
-        // Rattachement de nos balises au DOM
+
+        // Rattachement des balises crées au DOM
         filters.appendChild(buttonElement);
-        
     })
 }
 
 // Fonction qui affiche les filtres sur la page web
+
 renderFilters(categories)
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
 // Fonction qui supprime la gallery
 
-function removeGallery () {
+function removeGallery() {
     document.querySelector(".gallery").innerHTML = "";
 }
 
@@ -101,18 +92,18 @@ function removeGallery () {
 // Fonction qui permet de selectionner le filtre
 
 function selectFilter(id) {
-	let filters = document.querySelectorAll(".filtersButton")
-	filters[event.target.dataset.id].classList.add("filtersButtonActivated")
+    let filters = document.querySelectorAll(".filtersButton")
+    filters[event.target.dataset.id].classList.add("filtersButtonActivated")
 }
 
 
-//  Fonction qui réinitialise les filtres (permet qu'ils soient tous déselectionnées)
+// Fonction qui réinitialise les filtres (permet qu'ils soient tous déselectionnées)
 
 function resetFilters() {
-	let filters = document.querySelectorAll(".filtersButton")
-	for (let compteur = 0; compteur < filters.length; compteur++) {
-		filters[compteur].classList.remove("filtersButtonActivated")
-	}
+    let filters = document.querySelectorAll(".filtersButton")
+    for (let compteur = 0; compteur < filters.length; compteur++) {
+        filters[compteur].classList.remove("filtersButtonActivated")
+    }
 }
 
 
@@ -139,24 +130,24 @@ function filterWorks(id) {
 const filterAll = document.querySelector(`.filtersButton[data-id="0"]`)
 
 filterAll.addEventListener("click", () => {
-    filterWorks (event.target.dataset.id)
+    filterWorks(event.target.dataset.id)
 })
 
 
 //  Gestion de l'évènement "click" sur le bouton filtre "Objets"
 
-const filterObjects = document.querySelector(`.filtersButton[data-id="1"]`)    
+const filterObjects = document.querySelector(`.filtersButton[data-id="1"]`)
 
 filterObjects.addEventListener("click", (event) => {
-    filterWorks (event.target.dataset.id)
-        })
-        
+    filterWorks(event.target.dataset.id)
+})
+
 //  Gestion de l'évènement "click" sur le bouton filtre "Appartements"
-        
+
 const filterApartment = document.querySelector(`.filtersButton[data-id="2"]`)
 
 filterApartment.addEventListener("click", (event) => {
-    filterWorks (event.target.dataset.id)
+    filterWorks(event.target.dataset.id)
 })
 
 
@@ -165,33 +156,13 @@ filterApartment.addEventListener("click", (event) => {
 const filterHotelsRestaurants = document.querySelector(`.filtersButton[data-id="3"]`)
 
 filterHotelsRestaurants.addEventListener("click", (event) => {
-    filterWorks (event.target.dataset.id)
+    filterWorks(event.target.dataset.id)
 })
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-                                                        
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////
-//  Etape du code
-////////////////////////
-filterObjects.addEventListener("click", (event) => {
-    // console.log(event.target.dataset.id)
-    // console.log(works)
-    const worksFiltres = works.filter(work=>work.categoryId == event.target.dataset.id) //=> Attention: "==" différent "==="  => les valeurs n'ont pas 
-    //                                                                                  // le même type : "string" différent d' "objet"
 
-                        // Code identique:  // const worksFiltres = works.filter(work => {
-                                            //     return work.categoryId == event.target.dataset.id
-                                            // })
 
-                                            // const worksFiltres = works.filter(function (work) {
-                                            //     return work.categoryId == event.target.dataset.id
-                                            // })
-    removeGallery ()
-    renderWorks(worksFiltres)
-})
-//////////////////////////////////////////////////////////////////////////////////////////////////////
 
